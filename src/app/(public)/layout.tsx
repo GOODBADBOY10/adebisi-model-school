7b0175c74faf66a898d8f46f931e8422c8348c32
GOODBADBOY10 <selfmanademola@gmail.com>
2025-10-02 10:00:33 +0100

    fix: rename the title of the website

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,8 +4,8 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: 'School Portal',
-  description: 'Student and admin management system',
+  title: 'Adebisi Model School',
+  description: 'Adebisi Model School',
 }
 
 
@@ -21,18 +21,4 @@ export default function RootLayout({
       <Footer />
     </div>
   );
-  // return (
-  //   <html lang="en">
-  //     <body className="flex flex-col min-h-screen">
-  //       {/* Header will always be at the top */}
-  //       <Header />
-
-  //       {/* Main page content */}
-  //       <main className="flex-1">{children}</main>
-
-  //       {/* Footer will always be at the bottom */}
-  //       <Footer />
-  //     </body>
-  //   </html>
-  // );
 }
